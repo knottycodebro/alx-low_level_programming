91d380d13d1c29bfc470fd5e28c9a8ae8bfb557a
knottycodebro <bensantify@outlook.com>
2023-02-22 13:24:49 -0800

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,11 +10,15 @@ void jack_bauer(void)
 	int hour, min;
 
 	for (hour = 0; hour < 24; hour++)
+	{
 		for (min = 0; min < 59; min++)
-			_putchar((hours / 10) + 48);
-			_putchar((hours % 10) + 48);
+		{
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
 			_putchar(':');
 			_putchar((min / 10) + 48);
 			_putchar((min / 10) + 48);
 			_putchar('\n');
+		}
+	}
 }
