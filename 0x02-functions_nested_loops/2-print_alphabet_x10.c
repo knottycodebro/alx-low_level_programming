db97bc90447fd0836a89441f52d9994e1770cc12
knottycodebro <bensantify@outlook.com>
2023-02-22 09:12:13 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: prints the alphabet 10x with _putchar function
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int times = 10, i;
+	char alpha = 'a';
+
+	/*I'm using for loop together with while loop for this task😃😉*/
+	for (i = 0; i < times; i++)
+	{
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+	}
