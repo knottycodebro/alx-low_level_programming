f095b033ac795e5eaaffb2c2e29b6029b63c9823
knottycodebro <bensantify@outlook.com>
2023-02-22 14:35:56 -0800

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - add two n together
+ *
+ * @a: accepts an integer as an input
+ *
+ * @b: accepts an integer as an input
+ *
+ * Return: the result
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
