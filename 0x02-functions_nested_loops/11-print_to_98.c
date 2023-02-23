3462925efc89adb50445f46f0f1465eca7c39528
knottycodebro <bensantify@outlook.com>
2023-02-23 13:21:29 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,14 +15,14 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-		for (i = 0; i < 98; i++)
+		for (i = n; i < 98; i++)
 		{
 			printf("%d, ", i);
 		}
 	}
 	else if (n > 98)
 	{
-		for (i = 0; i > 98; i--)
+		for (i = n; i > 98; i--)
 		{
 			printf("%d, ", i);
 		}
