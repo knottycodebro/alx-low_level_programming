aeb0a5e44459483da01351994a03f7c6e417dfdc
knottycodebro <bensantify@outlook.com>
2023-02-25 07:06:19 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * main - Entry point
+ *
+ * @c: takes alphabet as an input
+ *
+ * Return: Always 1 (success)
+ */
+
+int main(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
